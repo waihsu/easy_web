9085b6a66fc2113295dd6a8426e2c1dd4ad9fd59
waihsu <hsuw647@gmail.com>
2024-05-27 14:05:18 +0630

    fix public web url change

--- src/components/component/herosection-v1.tsx
+++ src/components/component/herosection-v1.tsx
@@ -74,7 +74,7 @@ export function HeroSectionV1({ section }: { section: sections }) {
         <div className="mt-10">
           <Link
             className={buttonVariants({ variant: "default" })}
-            href={new URL(`https://${section.link}`)}
+            href={section.link ? section.link : "#"}
           >
             Get Started
           </Link>
